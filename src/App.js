ec0ff876523b55f1eaab0220eaa2cb2a49d59555
Cristhian Rodriguez <cristhianr.0912@gmail.com>
2022-11-08 06:26:56 -0500

    Setting 404 error (to be completed)

--- src/App.js
+++ src/App.js
@@ -4,19 +4,48 @@ import Searcher from "./Components/Searcher";
 import getGitHubUser from "./services/users";
 
 function App() {
-
+  console.log('1')
   const [inputUser, setInputUser] = useState('octocat');
+  console.log('2')
   const [userState, setUserState] = useState(inputUser);
+  // const [notFound, setNotFound] = useState(false);
+  console.log('3')
 
   const getUser = async (user) => {
+    console.log('8')
     const response = await getGitHubUser(inputUser);
-    console.log(response);
+    console.log('9')
+
+    if(userState === 'octocat') {
+      // localStorage.setItem('octocat', response);
+      console.log('10')
+    }
+
+    if(response.message === 'Not Found') {
+      console.log('11')
+      // const {octocat} = localStorage;
+      // setUserState(octocat);
+      // setNotFound(true);
+    } else {
+      console.log('12')
+      setUserState(response);
+      console.log('13')
+      // setNotFound(false);
+    }
+    console.log('14')
   }
 
+  console.log('4')
+    
   useEffect(() => {
+    console.log('5')
     getUser(inputUser);
+    console.log('6');
+    // console.log(userState);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [inputUser])
+  }, [inputUser]);
+
+  console.log('7')
 
   return (
     <Container sx={{
